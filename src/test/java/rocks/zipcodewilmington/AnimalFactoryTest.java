a22bc6ea1b9c0695380c34ed073b13ec48c4c9d6
anudeep-chennuri <anudeep.chennuri@gmail.com>
2023-07-28 12:29:22 -0400

    [completed] AnimalFactoryTest

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,31 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void testCreateDog() {
+        AnimalFactory animalFactory = new AnimalFactory();
+        Animal animal = animalFactory.createDog("Doggo", Date);
+        Assert.assertEquals("Doggo",Date);
+    }
+
+    @Test
+    public void testCreateCat() {
+        AnimalFactory animalFactory = new AnimalFactory();
+        Animal animal = animalFactory.createDog("Kitty", Date);
+        Assert.assertEquals("Kitty",Date);
+    }
+
 }
